bca51f402b7f55e84ce6e2d34791f79058efe74e
Monalisa Burma <122416015+monalisaburma@users.noreply.github.com>
2024-01-07 23:48:43 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ apt-get update
 apt-get install -y libmysqlclient-dev
 echo "Installation Starting........."
 # Install Python dependencies, including mysqlclient
-pip install --no-use-pep517 mysqlclient
-pip install -r requirements.txt  # or however you manage your dependencies
+pip3 install --upgrade pip
+pip3 install -r requirements.txt  # or however you manage your dependencies
 python app.py
 
 # Add any other deployment steps here
