81ad7e68cd97c57157e96d0365e4a442814954c6
Monalisa Burma <122416015+monalisaburma@users.noreply.github.com>
2024-01-08 01:38:07 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,16 @@
 
 # Install MySQL development headers and libraries
 apt-get update
-apt-get install -y python3-dev default-libmysqlclient-dev build-essential
+# apt-get install -y python3-dev default-libmysqlclient-dev build-essential
 echo "Installation Starting........."
-sudo apt-get update && apt-get upgrade python-pip
-sudo yum install python-pip
-sudo yum update python-pip
+#sudo apt-get update && apt-get upgrade python-pip
+#sudo yum install python-pip
+#sudo yum update python-pip
 # Install Python dependencies, including mysqlclient
-pip3 install --upgrade pip
-python -m pip install --upgrade pip
-python3 -m pip install --upgrade pip
-pip3 install git+https://github.com/PyMySQL/mysqlclient-python
+#pip3 install --upgrade pip
+#python -m pip install --upgrade pip
+#python3 -m pip install --upgrade pip
+# pip3 install git+https://github.com/PyMySQL/mysqlclient-python
 pip3 install -r requirements.txt  # or however you manage your dependencies
 python app.py
 
