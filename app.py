362ad4f446fe781c266cb2e3f31a5dc70dc423c0
Monalisa Burma <122416015+monalisaburma@users.noreply.github.com>
2024-01-08 23:35:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__, template_folder='templates')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'mysql+pymysql://root:876543@127.0.0.1:3306/user_login_info?charset=utf8mb4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:876543@127.0.0.1:3306/user_login_info?charset=utf8mb4'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
