6e359e811d5985f22e1c1685508ee19e2ce40158
Monalisa Burma <122416015+monalisaburma@users.noreply.github.com>
2024-01-09 14:56:14 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,9 @@ echo "Installation Starting........."
 #python -m pip install --upgrade pip
 #python3 -m pip install --upgrade pip
 # pip3 install git+https://github.com/PyMySQL/mysqlclient-python
+
+echo "gunicorn==20.1.0" >> requirements.txt
+
 export DATABASE_URL=mysql+pymysql://ujmpxoqqhngyt0gb:1OIbryg4bnDgFbT34umw@bpvu52ygesedno1kez8l-mysql.services.clever-cloud.com:3306/bpvu52ygesedno1kez8l
 pip3 install -r requirements.txt  # or however you manage your dependencies
 python app.py
