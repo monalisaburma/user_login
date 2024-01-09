66385c964244ba49935db3d6a127e3948874cc6b
Monalisa Burma <122416015+monalisaburma@users.noreply.github.com>
2024-01-09 19:30:32 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,9 @@
-#!/bin/bash
-
-# Install MySQL development headers and libraries
-# apt-get update
-# apt-get install -y python3-dev default-libmysqlclient-dev build-essential
 echo "Installation Starting........."
-#sudo apt-get update && apt-get upgrade python-pip
-#sudo yum install python-pip
-#sudo yum update python-pip
-# Install Python dependencies, including mysqlclient
-#pip3 install --upgrade pip
-#python -m pip install --upgrade pip
-#python3 -m pip install --upgrade pip
-# pip3 install git+https://github.com/PyMySQL/mysqlclient-python
-# pip3 install gunicorn==20.1.0
-
-echo "gunicorn==20.1.0" >> requirements.txt
-
 export DATABASE_URL=mysql+pymysql://ujmpxoqqhngyt0gb:1OIbryg4bnDgFbT34umw@bpvu52ygesedno1kez8l-mysql.services.clever-cloud.com:3306/bpvu52ygesedno1kez8l
-pip3 install -r requirements.txt  # or however you manage your dependencies
-python app.py
-printenv
+pip3 install -r requirements.txt
 
+# Install Waitress
+pip3 install waitress
 
-# Add any other deployment steps here
-# ...
-
-# Example: Build your site or start your server
-# npm run build  # replace with your build command
+# Start the Flask app using Waitress
+waitress-serve --host=0.0.0.0 --port=$PORT app:app
