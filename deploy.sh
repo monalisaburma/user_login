6dd4144de0a87fd4c68e5d280a4cfd48ec4cd2c9
Monalisa Burma <122416015+monalisaburma@users.noreply.github.com>
2024-01-09 19:40:20 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 echo "Installation Starting........."
+
+# Set environment variables
 export DATABASE_URL=mysql+pymysql://ujmpxoqqhngyt0gb:1OIbryg4bnDgFbT34umw@bpvu52ygesedno1kez8l-mysql.services.clever-cloud.com:3306/bpvu52ygesedno1kez8l
+export PORT=5000  # Or the port number you want to use
+
+# Install Python dependencies
 pip3 install -r requirements.txt
 
 # Install Waitress
 pip3 install waitress
 
 # Start the Flask app using Waitress
-waitress-serve --host=0.0.0.0 --port=$PORT app:app
+waitress-serve --listen=*:$PORT --call app:app
