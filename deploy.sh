14fe29c5aee4f87bdea2ac9b7cb648dfa74c53e8
Monalisa Burma <122416015+monalisaburma@users.noreply.github.com>
2024-01-09 20:06:54 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 echo "Installation Starting........."
 export DATABASE_URL=mysql+pymysql://ujmpxoqqhngyt0gb:1OIbryg4bnDgFbT34umw@bpvu52ygesedno1kez8l-mysql.services.clever-cloud.com:3306/bpvu52ygesedno1kez8l
-export PORT=5001  # Or the port number you want to use
+export PORT=5000 # Or the port number you want to use
 
 # Install dependencies
 pip3 install -r requirements.txt
